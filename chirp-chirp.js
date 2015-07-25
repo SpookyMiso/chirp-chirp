1674a1080af92492501f23d3ae893748bc2f9aee
Jenn L <jenn.lazariuk@gmail.com>
2015-07-25 11:50:03 -1000

    wrote first test

---
+++ chirp-chirp.js
@@ -0,0 +1,4 @@
+function chirpRecursion (num) {
+
+}
+
---
+++ test/test-script.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+describe('chirpRecursion', function() {
+
+  it('should be a function', function() {
+    expect(chirpRecursion).to.be.a('function');
+  });
+});
